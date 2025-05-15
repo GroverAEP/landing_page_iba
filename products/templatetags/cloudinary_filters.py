3ed56de75585d0a13c9f401f4086372e9b78d81f
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-14 21:42:22 -0500

    aumentando la calidad a 500px

--- products/templatetags/cloudinary_filters.py
+++ products/templatetags/cloudinary_filters.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.filter
-def cloudinary_transform(url, params="w_400,h_400,c_fill,f_auto,q_auto"):
+def cloudinary_transform(url, params="w_500,h_500,c_fill,f_auto,q_auto"):
     if not url:
         return ""
     parts = url.split("/upload/")
