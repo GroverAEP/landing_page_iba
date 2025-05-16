7da6277a89cb575f7c8a73536f2e7d9dc66155e4
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-16 11:10:53 -0500

    renderizando las iamgenes a 300px

--- products/templatetags/cloudinary_filters.py
+++ products/templatetags/cloudinary_filters.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.filter
-def cloudinary_transform(url, params="w_1000,h_1000,c_fill,f_auto,q_auto"):
+def cloudinary_transform(url, params="w_300,h_300,c_fill,f_auto,q_auto"):
     if not url:
         return ""
     parts = url.split("/upload/")
