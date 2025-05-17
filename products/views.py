a19bac7618b63044638841ca3e40eee95a57ba73
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-17 14:42:06 -0500

    corriengo el codigo de las consultas

--- products/views.py
+++ products/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Producto, Categoria
-from urllib.parse import quote_plus
 from django.db.models import Q
 # Create your views here.
 import unicodedata
-from urllib.parse import urlencode
 
 def normalize(text):
     if text is None:
@@ -21,36 +19,21 @@ def catalog_products(request):
     products_total_count = products.count()
     # Obtener solo las categorías que tienen productos asignados
     categories = Categoria.objects.filter(producto__isnull=False).distinct()  # Filtrar categorías con productos
-    
     # Filtrar los productos si hay una consulta de búsqueda (por nombre o marca)
     query = request.GET.get('q', '').strip()  # Obtener el valor de búsqueda del parámetro 'q'
-    
     # Si la consulta de búsqueda no está vacía, aplicamos los filtros
     if query:
         words = query.split()
-        normalized_words = [normalize(word) for word in words]
-
-        filtered_products = []
-
-        for product in products:
-            name = normalize(product.name)
-            brand = normalize(product.brand)
-            category = normalize(product.category.name)
-
-            # Verificamos si alguna de las palabras aparece en alguno de los campos
-            any_word_matches = False
-            for word in normalized_words:
-                if word in name or word in brand or word in category:
-                    any_word_matches = True
-                    break
-
-            # Si alguna palabra coincide, añadimos el producto
-            if any_word_matches:
-                filtered_products.append(product)
-
-        products = filtered_products
-        
-        
+        q_objects = Q()
+        for word in words:
+            normalized_word = normalize(word)
+            q_objects |= (
+                Q(name__icontains=normalized_word) |
+                Q(brand__icontains=normalized_word) |
+                Q(category__name__icontains=normalized_word)
+            )
+        products = Producto.objects.filter(q_objects)
+    
     selected_category = None
     # Filtrar productos por categoría seleccionada si es necesario
     category_id = request.GET.get('category')
@@ -60,6 +43,7 @@ def catalog_products(request):
             selected_category = Categoria.objects.get(id=category_id)
         except Categoria.DoesNotExist:
             selected_category = None
+    
     # Ordenar los productos por precio si se pasa el parámetro 'order_by_price'
     order_by = request.GET.get('order_by_price')
     if order_by == 'barato':
