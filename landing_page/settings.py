db8c14d2f734ccff205aa7bde6dd7340ed9008bb
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-21 02:14:42 -0500

    actualizando los cambios ala conexion de neon

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -85,13 +85,33 @@ WSGI_APPLICATION = "landing_page.wsgi.application"
 # }
 
 # DATABASE CONEXION A NEON
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # aqui debo hacer la conexion respectiva a neon 
+#         default='postgresql://neondb_owner:npg_Q3dsFhVKfri2@ep-winter-sky-acuumc1h-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
+# Add these at the top of your settings.py
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+load_dotenv()
+
+# Replace the DATABASES section of your settings.py with this
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
-    'default': dj_database_url.config(
-        # aqui debo hacer la conexion respectiva a neon 
-        default='postgresql://neondb_owner:npg_Q3dsFhVKfri2@ep-winter-sky-acuumc1h-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
