5fd7ff13f63799c85a5cab8adebb3cc03c9e3834
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-20 22:13:37 -0500

    migrando la bd a Neon

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 import cloudinary
 import dj_database_url
-
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # con la siguiente configuracion puedo trabajar con los datos de la bd remota en mi loca,
 # pero si cambiamos de bd remota a local trabajmos con ella exitosamente
@@ -76,10 +77,19 @@ WSGI_APPLICATION = "landing_page.wsgi.application"
 # }
 
 # Esta configruacion es cuando se conecta de amnera remota a nuestra bd
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default="postgresql://ibafex:4ngr02QYxXXylde8cq6fdsZU6VmRwisD@dpg-d057chi4d50c73ahcgs0-a.oregon-postgres.render.com/bd_ibafex",
+#         conn_max_age=600,
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default="postgresql://ibafex:4ngr02QYxXXylde8cq6fdsZU6VmRwisD@dpg-d057chi4d50c73ahcgs0-a.oregon-postgres.render.com/bd_ibafex",
+    'default': dj_database_url.parse(
+        # aqui debo hacer la conexion respectiva a neon 
+        'postgresql://neondb_owner:npg_Q3dsFhVKfri2@ep-winter-sky-acuumc1h-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
         conn_max_age=600,
+        ssl_require=True
     )
 }
 
