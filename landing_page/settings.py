f4b13bc53f062798bd38e9fa3ff5088f47ee3c17
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-21 03:46:48 -0500

    Desplegado con la BD de render

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = "landing_page.wsgi.application"
 # }
 
 # DATABASE CONEXION A RENDER
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default="postgresql://ibafex:4ngr02QYxXXylde8cq6fdsZU6VmRwisD@dpg-d057chi4d50c73ahcgs0-a.oregon-postgres.render.com/bd_ibafex",
-#         conn_max_age=600,
-#     )
-# }
-
-# DATABASE CONEXION A NEON
 DATABASES = {
-    'default': dj_database_url.parse(
-        # aqui debo hacer la conexion respectiva a neon 
-        'postgresql://neondb_owner:npg_Q3dsFhVKfri2@ep-winter-sky-acuumc1h-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
+    'default': dj_database_url.config(
+        default="postgresql://ibafex:4ngr02QYxXXylde8cq6fdsZU6VmRwisD@dpg-d057chi4d50c73ahcgs0-a.oregon-postgres.render.com/bd_ibafex",
         conn_max_age=600,
-        ssl_require=True
     )
 }
+
+# DATABASE CONEXION A NEON
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         # aqui debo hacer la conexion respectiva a neon 
+#         'postgresql://neondb_owner:npg_Q3dsFhVKfri2@ep-winter-sky-acuumc1h-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
 # Add these at the top of your settings.py
 
 # DATABASES = {
