3a0cb8053b2d94f57f039798fb07d6b73189c6aa
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-05-23 05:18:16 -0500

    comentando la funcionalidad de preloader img para implementarlo mas adelante

--- products/templatetags/cloudinary_filters.py
+++ products/templatetags/cloudinary_filters.py
@@ -1,9 +1,9 @@
 from django import template
 
 register = template.Library()
-
+# 600px es lo recomendado
 @register.filter
-def cloudinary_transform(url, params="w_700,h_700,c_fill,f_auto,q_auto"):
+def cloudinary_transform(url, params="w_300,h_300,c_fill,f_auto,q_auto"):
     if not url:
         return ""
     parts = url.split("/upload/")
