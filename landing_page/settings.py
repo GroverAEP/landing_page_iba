a8b996e66448f3ec812a070f32eb5466560c65df
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-08-26 20:15:16 -0500

    conectandome a neon con la cuenta de Pillamarlon 2

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'neondb',
         'USER': 'neondb_owner',
-        'PASSWORD': 'npg_T3A4VFPKEcLi',
-        'HOST': 'ep-damp-meadow-afdzvw41-pooler.c-2.us-west-2.aws.neon.tech',
+        'PASSWORD': 'npg_H5uwhjm9Yqnf',
+        'HOST': 'ep-summer-sunset-afx0445l-pooler.c-2.us-west-2.aws.neon.tech',
         'PORT': '5432',
         'CONN_MAX_AGE': 600,
         'OPTIONS': {
