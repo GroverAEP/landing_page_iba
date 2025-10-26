dc895603d41f81ca76d512d94fc82a5b653e48c0
MarlonXpp <InfoMarlon.Pillaca@gmail.com>
2025-10-26 02:42:31 -0500

    conectando a la misma base de datos

--- landing_page/settings.py
+++ landing_page/settings.py
@@ -91,32 +91,18 @@ WSGI_APPLICATION = "landing_page.wsgi.application"
 # }
 
 
-# DATABASE CONEXION A NEON - PILLAMARLON
+
+
+
+
+# DATABASE CONEXION A NEON - INFOPILLACA - ibafex1
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'neondb',
 #         'USER': 'neondb_owner',
-#         'PASSWORD': 'npg_H5uwhjm9Yqnf',
-#         'HOST': 'ep-summer-sunset-afx0445l.c-2.us-west-2.aws.neon.tech',
-#         'PORT': '5432',
-#         'CONN_MAX_AGE': 600,
-#         'OPTIONS': {
-#             'sslmode': 'require',
-#             'options': '-c search_path=public',   # 👈 esto es lo que faltaba
-#         },
-#     }
-# }
-
-
-# DATABASE CONEXION A NEON - INFOPILLACA
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ibafex_bd',
-#         'USER': 'ibafex_bd_owner',
-#         'PASSWORD': 'npg_9iaWAcweIb2o',
-#         'HOST': 'ep-restless-union-a6yj00of-pooler.us-west-2.aws.neon.tech',
+#         'PASSWORD': 'npg_cNKSIHpf9v1s',
+#         'HOST': 'ep-super-hall-afw1uarg-pooler.c-2.us-west-2.aws.neon.tech',
 #         'PORT': '5432',
 #         'CONN_MAX_AGE': 600,
 #         'OPTIONS': {
@@ -125,14 +111,15 @@ WSGI_APPLICATION = "landing_page.wsgi.application"
 #     }
 # }
 
-# DATABASE CONEXION A NEON - INFOPILLACA - ibafex1
+
+# DATABASE CONEXION A NEON - INFOPILLACA - ibafex2
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'neondb',
         'USER': 'neondb_owner',
-        'PASSWORD': 'npg_cNKSIHpf9v1s',
-        'HOST': 'ep-super-hall-afw1uarg-pooler.c-2.us-west-2.aws.neon.tech',
+        'PASSWORD': 'npg_PbML6quVT7fo',
+        'HOST': 'ep-summer-paper-afaep138-pooler.c-2.us-west-2.aws.neon.tech',
         'PORT': '5432',
         'CONN_MAX_AGE': 600,
         'OPTIONS': {
@@ -141,33 +128,6 @@ DATABASES = {
     }
 }
 
-
-# DATABASE CONEXION A NEON - GHOSTH
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'neondb',
-#         'USER': 'neondb_owner',
-#         'PASSWORD': 'npg_Q5xfMSD2Wdpn',
-#         'HOST': 'ep-divine-lake-affqyq3x-pooler.c-2.us-west-2.aws.neon.tech',
-#         'PORT': '5432',
-#         'CONN_MAX_AGE': 600,
-#         'OPTIONS': {
-#             'sslmode': 'require',
-#         },
-#     }
-# }
-
-
-
-# DATABASES = {
-#     'default': dj_database_url.parse(
-#         # aqui debo hacer la conexion respectiva a neon 
-#         'postgresql://neondb_owner:npg_Q3dsFhVKfri2@ep-winter-sky-acuumc1h-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
-# }
 
 # DATABASE CONEXION A RAILWAY
 # DATABASES = {
